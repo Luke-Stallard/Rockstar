e0bbc08964d4b1e487e888d1d97d368f5119e70f
Luke <hello.lukestallard@gmail.com>
2026-09-09 12:28:27 +1200

    start

---
+++ history_test.py
@@ -0,0 +1,17 @@
+history = [
+    {
+        "round": 1,
+        "player": "Rock",
+        "opponent": "Paper",
+        "round_winner": "Opp"
+    },
+    {
+        "round": 2,
+        "player": "Scissors",
+        "opponent": "Paper",
+        "round_winner": "Player"
+    }
+]
+print(history[0]["opponent"])
+print(history[-1])
+print(history[-1]["opponent"])
---
+++ rockstar_bot.py
@@ -0,0 +1,14 @@
+history = [
+    {
+        "round": 1,
+        "player": "Rock",
+        "opponent": "Paper",
+        "round_winner": "Opp"
+    },
+    {
+        "round": 2,
+        "player": "Scissors",
+        "opponent": "Paper",
+        "round_winner": "Player"
+    }
+]
